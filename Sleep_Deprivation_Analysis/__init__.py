c727c1338f35ad3ab616756e06da6c085cf0e5f2
r-admuyiwa <ridwanade@hotmail.com>
2026-02-17 18:03:13 +0000

    linear regression grapgh of each column added

--- Sleep_Deprivation_Analysis/__init__.py
+++ Sleep_Deprivation_Analysis/__init__.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-df= pd.read_csv("sleep_deprevation.csv")
+df= pd.read_csv("./sleep_deprevation.csv")
 df.columns = df.columns.str.strip()
 
 target_col = "PVT_Reaction_Time"
@@ -28,6 +28,14 @@ for col in df.columns:
     print("Slope:" , model.coef_[0])
     print("Intercept:" , model.intercept_)
     print()
+
+    plt.figure()
+    plt.scatter(x,y)
+    plt.plot(x , model.predict(x), 'r-')
+    plt.xlabel(col)
+    plt.ylabel(target_col)
+    plt.title(f"{col} vs. {target_col}")
+    plt.show()
 
 def divide (x, y):
     z = x/ y
