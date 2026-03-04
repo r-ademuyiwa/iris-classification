b15d766af9e18faf62d75d10913c697750ff6ffc
r-admuyiwa <ridwanade@hotmail.com>
2026-03-04 02:33:51 +0000

    added KNN package

--- Flower_Classification/Class_code.py
+++ Flower_Classification/Class_code.py
@@ -10,7 +10,11 @@ import typer  # type: ignore[import-untyped]
 from typing import Annotated  # type: ignore[import-untyped]
 
 # importing the method of splitting the test data.
-from sklearn.model_selection import train_test_split  # type: ignore[import-untyped]
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
 
 # importing the model for scaling the data so that everything is in the same scale
 from sklearn.preprocessing import StandardScaler  # type: ignore[import-untyped]
@@ -30,15 +34,15 @@ def main(
     input_file: Annotated[Path, typer.Argument()],
 ) -> None:
     # reading the CSV as a dataframe and reading the columns
-    df: pd.Dataframe = pd.read_csv(input_file)
+    df: pd.DataFrame = pd.read_csv(input_file)
     df.column = df.columns.str.strip()
     # making sure that from every row in every column, the values inside is an integer
     # basically making an array
     X: np.ndarray = df.iloc[:, :-1].values
     y: np.ndarray = df.iloc[:, -1].values
 
-    print(X)
-    print(y)
+    print(X.shape)
+    print(y.shape)
     # printing out the file name, what type of file, and the directory of the file
     print(input_file.name, input_file.stem, input_file.suffix, input_file.resolve())
 
@@ -47,10 +51,25 @@ def main(
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
     # 0.25 x 0.8 = 0.2
     scaler = StandardScaler()
-    print(scaler.fit(X_train))
-    print(scaler.mean_)
-    print(scaler.transform(X_train))
+    X_train_scaled= scaler.fit_transform(X_train)
+    X_test_scaled= scaler.transform(X_test)
+    X_val_scaled= scaler.transform(X_val)
+
+    # print(scaler.mean_)
+    # print(scaler.transform(X_train))
     # print(scaler.transform([[2, 2]]))
+
+    knn = KNeighborsClassifier(n_neighbors=5)
+
+    knn.fit(X_train_scaled, y_train)
+    y_pred = knn.predict(X_test_scaled)
+    y_val_pred = knn.predict(X_val_scaled)
+    # prediction = knn.predict(df.values.reshape(1, -1))
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+
+    print(confusion_matrix(y_val, y_val_pred))
+    print(classification_report(y_val, y_val_pred))
 
 
 # @app.command()
